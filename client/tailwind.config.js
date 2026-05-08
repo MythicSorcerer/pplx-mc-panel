b39dee4cb1d59d7ae2ba752cde7cd0e1ab02833c
mythicsorcerer <mythicsorcerer@gmail.com>
2026-05-08 10:40:42 -0700

    fix: add custom colors to tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        emerald: "#39d98a",
+        mint: "#8ff0b5",
+        moss: "#4a5d53",
+        coal: "#0c1214",
+        redstone: "#e54d2e",
+      },
+    },
+  },
 }
